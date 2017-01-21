774b2bf0d5db8f255f46535093cff5a3536efd9b
Tokusaburo Hiramoto <tra.tokusabu@gmail.com>
2017-01-21 10:42:29 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,55 @@
+class ContactsController < ApplicationController
+  before_action :set_contact, only: [:edit, :update, :destroy]
+  
+  def index
+    @contacts=Contact.all
+  end
+  
+  def new
+     if params[:back]
+       @contact=Contact.new(contacts_params)
+     else
+     @contact =Contact.new
+     end
+  end
+  
+  def create
+    @contact = Contact.new(contacts_params)
+    if @contact.save
+    redirect_to root_path,notice:"お問い合わせありがとうございました！"
+    else
+     render 'new'
+   end
+  end
+  
+  def edit
+    @contact = Contact.find(params[:id])
+  end
+  
+  def update
+    @contact = Contact.find(params[:id])
+    @contact.update(contacts_params) 
+    redirect_to new_contact_path, notice: "お問い合わせをアップデートしました！"
+  end
+  
+  def destroy
+    @contact = Contact.find(params[:id])
+    @contact.destroy
+    redirect_to new_contact_path, notice: "お問い合わせを削除しました！"
+  end
+  
+  def confirm
+   @contact=Contact.new(contacts_params)
+   render:new if@contact.invalid?
+  end
+  
+  private
+    def contacts_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+    
+  def set_contact
+    @contact=Contact.find(params[:id])
+  end
+  
+end
