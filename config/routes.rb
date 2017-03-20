cf359157da0c25634320a321a0c7d24e89367152
Tokusaburo Hiramoto <tra.tokusabu@gmail.com>
2017-03-20 09:33:21 +0000

    “AAA”

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
   resources :blogs, only: [:index, :new , :create ,:edit ,:update ,:destroy]do
     collection do
@@ -15,6 +16,10 @@ Rails.application.routes.draw do
   
   root 'top#index'
   
+  if Rails.env.development?
+  mount LetterOpenerWeb::Engine, at: "/letter_opener"
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
