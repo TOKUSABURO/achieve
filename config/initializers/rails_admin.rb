fd45438b2c50d691a64f656f648dffd2c5d482e0
Tokusaburo Hiramoto <tra.tokusabu@gmail.com>
2017-03-25 08:32:17 +0000

    “コミットの内容”

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,7 +4,7 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-  　warden.authenticate! scope: :user
+   warden.authenticate! scope: :user
   end
   config.current_user_method(&:current_user)
 
