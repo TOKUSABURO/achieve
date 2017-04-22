7d28a57ff029e3f0ac4b854eb7b854120e6f23fc
John Doe <johndoe@example.com>
2017-04-22 09:24:08 +0000

    DIVER

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,19 +1,20 @@
 class BlogsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_blog, only: [:edit, :update, :destroy]
-  
+
   def index
     @blogs=Blog.all
+    raise
   end
-  
+
   def new
    if params[:back]
      @blog=Blog.new(blogs_params)
    else
-     @blog =Blog.new 
+     @blog =Blog.new
    end
   end
-  
+
   def create
     @blog = Blog.new(blogs_params)
     @blog.user_id = current_user.id
@@ -23,34 +24,34 @@ class BlogsController < ApplicationController
      render 'new'
     end
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     @blog = Blog.find(params[:id])
-    @blog.update(blogs_params) 
+    @blog.update(blogs_params)
     redirect_to new_blog_path, notice: "ブログをアップデートしました！"
   end
-  
+
   def destroy
     @blog = Blog.find(params[:id])
     @blog.destroy
     redirect_to new_blog_path, notice: "ブログを削除しました！"
   end
-  
+
   def confirm
     @blog=Blog.new(blogs_params)
     render:new if@blog.invalid?
   end
-  
+
   private
-  
+
   def blogs_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
   # idをキーとして値を取得するメソッド
   def set_blog
     @blog = Blog.find(params[:id])
