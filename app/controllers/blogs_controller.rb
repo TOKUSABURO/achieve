2c7b8c7d61ef16c41640da3fbbd4566a834d222f
John Doe <johndoe@example.com>
2017-05-15 11:48:22 +0000

    AAA

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,6 +6,12 @@ class BlogsController < ApplicationController
     @blogs=Blog.all
   end
 
+  # showアククションを定義します。入力フォームと一覧を表示するためインスタンスを2つ生成します。
+  def show
+    @comment = @blog.comments.build
+    @comments = @blog.comments
+  end
+
   def new
    if params[:back]
      @blog=Blog.new(blogs_params)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,20 @@
+
 100.times do |n|
+  name = "test"
   email = Faker::Internet.email
   password = "password"
+  provider = Faker::Internet.email
   User.create!(email: email,
+               name: name,
                password: password,
                password_confirmation: password,
+               uid: n,
+               provider: provider
+               )
+      t=User.last.id
+   Blog.create(
+               title: "iii",
+               content: "hoge",
+               user_id: t
                )
 end
