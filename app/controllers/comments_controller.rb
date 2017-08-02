a76f5c6375132abe648efef23fc0c2999e8eaa7c
John Doe <johndoe@example.com>
2017-08-02 03:50:36 +0000

    201708021250 correct pusher

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,9 @@ class CommentsController < ApplicationController
     @notification = @comment.notifications.build(user_id: @blog.user.id )
     respond_to do |format|
       if @comment.save
+        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.js { render :index }
+        format.html { render :new }
         unless @comment.blog.user_id == current_user.id
           Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'comment_created', {
           message: 'あなたの作成したブログにコメントが付きました'
@@ -14,10 +17,8 @@ class CommentsController < ApplicationController
         Pusher.trigger("user_#{@comment.blog.user_id}_channel", 'notification_created', {
         unread_counts: Notification.where(user_id: @comment.blog.user.id, read: false).count
         })
-        format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
-        format.js { render :index }
-      else
-        format.html { render :new }
+       else
+       format.html { render :new }
       end
     end
   end
